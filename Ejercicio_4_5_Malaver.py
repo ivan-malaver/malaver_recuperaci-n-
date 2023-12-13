b7eea0c85f4a8ef10ceb6f36308990fd3d292beb
ivan-malaver <malaver6@gmail.com>
2023-12-12 20:56:38 -0500

    Malaver.py

--- Ejercicio_4_5_Malaver.py
+++ Ejercicio_4_5_Malaver.py
@@ -1,42 +1,54 @@
-#comentario 
-#import
+
 
 from unittest import case
 
+palabras_ordenadas=str
 
-palabra=[]
-for i in range(10):
-    palabra=input(f"ingrese la palabra")
-    val= (i+1)
-palabra.append(val)
-#print(palabra)
-#comentario 
-def ordenar_por_tamaño(palabra):
-    return sorted(palabra,key=len,reverse=True)
-#comentario
+
+palabras=[]
+palabra=input("ingrese la palabra ")
+
+for i in range(0,5):
+    
+    palabra=input("ingrese la palabra ")
+    palabras.append(palabra)
+    i+1
+print(f"palabras de la lista", palabras)
+
+def ordenar_por_tamaño(palabras):
+ return sorted(palabras, key=len, reverse=True)
+
 while True:
-    print("\nSeleccione una opcion:")
-    print("1.ordenar alfabeticamente:")
-    print("2.ordenar tamaño, de la mas larga a la mas corta:")
-    print("3.ordenar aleatoricamente:")
-    print("4.ordenar inverso al ingresar:")
-    print("5.ordenar igal al ingresar:")
-    print("6.salir:")
-    seleccion=input("Elija una opcion:")
-    match=seleccion
-    case = "1"
-    palabras_rodenadas =sorted(palabra)
-    case = "2"
-    palabras_rodenadas =ordenar_por_tamaño(palabra)
-    case = "3"
-    import random
-    random.shufle(palabra)
-    palabras_rodenadas =palabra
-    case = "4"
-    palabras_rodenadas =list(reversed(palabra))
-    case = "5"
-    palabras_rodenadas =palabra
-    ordenar_por_tamaño(palabra)
+ print("\nSeleccione una opción:")
+ print("1. Orden alfabético de A-Z")
+ print("2. Orden longitud de la palabra")
+ print("3. Orden aleatorio noespecifico ")
+ print("4. Orden inverso al ingresado la primera palabra ingresada es la ultima en aparecer ")
+ print("5. igual al ingreso de las palabras ")
+ print("6. Salir del programa ")
+
+ seleccion = input(" opción de seleccion :" )
+ match seleccion:
+    case 1:
+        palabras_ordenadas = sorted(palabras)
+    case 2:
+        palabras_ordenadas = ordenar_por_tamaño(palabras)
+    case 3:
+        random.shuffle(palabras)
+        palabras_ordenadas = palabras
+    case 4:
+        palabras_ordenadas = list(reversed(palabras))
+    case 5:
+        palabras_ordenadas = palabras
+    case 6:
+        break
+    case _:
+        print("Opción no válida. Elija una opción del 1 al 6.")
+ continue
+
+print("\nPalabras ordenadas:")
+for palabra in palabras_ordenadas:
+ print(palabra)
     
     
     
